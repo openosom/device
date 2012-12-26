15fec7cecad5d4244954f440fcf6fcc9e57b8297
richard.nee.cn <richard.nee.cn@74e81aba-36f0-4891-8cfb-1815f9025a0c>
2012-12-26 11:16:14 +0000

    feeds/device/qemu-openmoko:[[BR]]  *添加脚本，运行openmoko QEMU模拟器
    git-svn-id: svn://svn.openwrt.org.cn/dreambox/feeds/device@586 74e81aba-36f0-4891-8cfb-1815f9025a0c

---
+++ qemu-openmoko/files/run-openmoko-emulator.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+
+BASE=$(dirname $0)
+QEMU_DIR=$BASE
+SCRIPTS_DIR=$QEMU_DIR/openmoko
+
+#1 setup environment
+. $SCRIPTS_DIR/env
+
+#2 download the most recent <u-boot>, <Kernel>, and <root filesystem> images
+$SCRIPTS_DIR/download.sh
+
+# which runs the emulator, loads u-boot into it 
+#	and then uses u-boot's capability to program the Flash memory 
+#	to install all the necessary parts of the system into the virtual Flash. 
+# It will also set up all the bootloading process 
+#	including a boot menu (ENTER is [AUX] and SPACE is [POWER]), splash, 
+#	u-boot environment and some default kernel parameters. 
+# If everything goes OK, the script should print a command which you can use to start using the emulator.
+if [ ! -e $SCRIPTS_DIR/$flash_base -o ! -e $SCRIPTS_DIR/$flash_image ]; then
+	$SCRIPTS_DIR/flash.sh
+fi
+
+#3 launches qemu and opens a unix socket for communication. All additional parameters are passed to qemu.
+$SCRIPTS_DIR/qemu-auto.sh
+
+
+
+#$SCRIPTS_DIR/qemu-upload.sh <filename>
+#$SCRIPTS_DIR/qemu-quit.sh
+#$SCRIPTS_DIR/qemu-gadget.sh
+
