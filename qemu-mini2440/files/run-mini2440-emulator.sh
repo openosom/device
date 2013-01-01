f34b0ec216382de5280b1d53501e401d8382ff74
richard.nee.cn <richard.nee.cn@74e81aba-36f0-4891-8cfb-1815f9025a0c>
2013-01-01 05:24:44 +0000

    feeds/device/qemu-mini2440 :[[BR]] *重新编写run-mini2440-emulator.sh脚本
    git-svn-id: svn://svn.openwrt.org.cn/dreambox/feeds/device@593 74e81aba-36f0-4891-8cfb-1815f9025a0c

--- qemu-mini2440/files/run-mini2440-emulator.sh
+++ qemu-mini2440/files/run-mini2440-emulator.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 #
 
-BASE="`pwd`"
+BASE=$(dirname $0)
 SCRIPTS_DIR=$BASE/qemu-mini2440/mini2440
 
-. $SCRIPTS_DIR/env
+cd $SCRIPTS_DIR
 
-# nand flash image
-if [ ! -e $SCRIPTS_DIR/$flash_base -o ! -e $SCRIPTS_DIR/$flash_image ]; then
-	$SCRIPTS_DIR/flash.sh $BASE
-fi
+# 1. QEMU enviroment
+. ./env
 
-if [ ! -e $BASE/u-boot.bin -a -e $SCRIPTS_DIR/u-boot.bin ]; then
-	ln -sv $SCRIPTS_DIR/u-boot.bin $BASE/u-boot.bin
+# 2. creat nand flash image, and write u-boot, kernel, rootfs into it.
+if [ ! -e $flash_base -o ! -e $flash_image ]; then
+./flash.sh ../..
 fi
 
 sleep 1
 
-$SCRIPTS_DIR/qemu-auto.sh
+# 3. run QEMU
+./qemu-auto.sh
 
