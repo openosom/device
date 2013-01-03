ae8a92baa38c51e32ff9ffeeb3b54a6477900252
richard.nee.cn <richard.nee.cn@74e81aba-36f0-4891-8cfb-1815f9025a0c>
2013-01-03 16:42:40 +0000

    feeds/device: [[BR]] *添加qemu-s3c24xx，基于qemu-mini2440，将会支持s3c2440/s3c2442/s3c2410
    git-svn-id: svn://svn.openwrt.org.cn/dreambox/feeds/device@594 74e81aba-36f0-4891-8cfb-1815f9025a0c

---
+++ qemu-s3c24xx/files/run-s3c2440-emulator.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+
+BASE=$(dirname $0)
+SCRIPTS_DIR=$BASE/qemu-s3c24xx/s3c2440
+
+cd $SCRIPTS_DIR
+
+# 1. QEMU enviroment
+. ./env
+
+# 2. creat nand flash image, and write u-boot, kernel, rootfs into it.
+if [ ! -e $flash_base -o ! -e $flash_image ]; then
+./flash.sh ../..
+fi
+
+sleep 1
+
+# 3. run QEMU
+./qemu-auto.sh
+
